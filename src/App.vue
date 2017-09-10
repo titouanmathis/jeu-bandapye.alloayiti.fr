e7bae8cfa273bd3c28c228d55f0eaa0938f2b262
Titouan Mathis <titouan@studiometa.fr>
2017-09-10 10:34:27 +0200

    Create basic game functionality

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-	<div id="app">
+	<div class="app">
 		<div class="row">
 			<div class="col">
 				<ul class="images">
-					<li class="images__item" v-for="image in images">
+					<li class="images__item" v-for="image in images" @drop="dropHandler(image)" @dragover.prevent @dragenter="dragenterHandler(image)" @dragleave="dragleaveHandler(image)" :class="image.classes.join(' ')">
 						<span class="images__name">{{ image.name }}</span>
 						<img class="images__img" :src="image.image.src" :width="image.image.width" :height="image.image.height" alt="?">
 					</li>
@@ -11,11 +11,9 @@
 			</div>
 			<div class="col">
 				<ul class="names">
-					<draggable v-model="names" :options="{ group: 'names' }" @start="isDragging = true" @end="isDragging = false">
-						<li class="names__item" v-for="name in names" :key="name.id">
-							<button type="button" class="names__btn">{{ name.name }}</button>
-						</li>
-					</draggable>
+					<li class="names__item" v-for="name in names" :key="name.id">
+						<span draggable="true" @dragstart="dragstartHandler(name)" @dragend="dragendHandler" class="names__btn">{{ name.name }}</span>
+					</li>
 				</ul>
 			</div>
 		</div>
@@ -23,20 +21,98 @@
 </template>
 
 <script>
-	import draggable from 'vuedraggable'
-	import instruments from './instruments'
-
+	import instruments from './data/instruments.yml'
 
 	export default {
 		name: 'app',
-		components: {
-			draggable
-		},
 		data() {
 			return {
 				isDragging: false,
-				names: instruments.slice(),
-				images: instruments.slice()
+				names: instruments.slice().sort(() => 0.5 - Math.random()),
+				images: instruments.slice().sort(() => 0.5 - Math.random()),
+				currentName: null,
+				classes: {
+					error: 'is-error',
+					success: 'is-success',
+					over: 'is-over'
+				}
+			}
+		},
+		methods: {
+
+			/**
+			 * Handler for the drop on a target
+			 *
+			 * @param  {Object} image The target object
+			 */
+			dropHandler(image) {
+				this.removeClass(image, this.classes.over)
+				if (this.currentName.id === image.id) {
+					this.addClass(image, this.classes.success)
+					const index = this.names.indexOf(this.currentName)
+					this.names.splice(index, 1)
+				} else {
+					this.addClass(image, this.classes.error)
+					setTimeout(() => {
+						this.removeClass(image, this.classes.error)
+					}, 500);
+				}
+			},
+
+			/**
+			 * Handler for the `dragstart` event
+			 *
+			 * @param  {Object} name The name's object
+			 */
+			dragstartHandler(name) {
+				this.currentName = name
+				this.isDragging = true
+			},
+
+			/**
+			 * Handler for the `dragend` event
+			 */
+			dragendHandler() {
+				this.isDragging = false
+			},
+
+			/**
+			 * Handler for the `dragenter` event
+			 *
+			 * @param  {Object} image The object of the image being entered
+			 */
+			dragenterHandler(image) {
+				this.addClass(image, this.classes.over)
+			},
+
+			/**
+			 * Handler for the `dragleave` event
+			 *
+			 * @param  {Object} image The object of the image being leaved
+			 */
+			dragleaveHandler(image) {
+				this.removeClass(image, this.classes.over)
+			},
+
+			/**
+			 * Helper to add a class name to a given object
+			 *
+			 * @param {Object} object    The object to add the class to
+			 * @param {String} className The class name to add
+			 */
+			addClass(object, className) {
+				object.classes.push(className)
+			},
+
+			/**
+			 * Helper to remove a class name from a given object
+			 *
+			 * @param  {Object} object    The object to remove the class from
+			 * @param  {String} className The class name to remove
+			 */
+			removeClass(object, className) {
+				const index = object.classes.indexOf(className)
+				object.classes.splice(index, 1)
 			}
 		}
 	}
@@ -77,7 +153,7 @@
 
 	.images {
 		display: flex;
-		width: 600px;
+		max-width: 600px;
 		flex-wrap: wrap;
 		margin: 0;
 		padding-left: 0;
@@ -91,6 +167,18 @@
 		flex: 1 0 33.33%;
 		padding: 1em;
 		border: 1px solid #eee;
+
+		&.is-over {
+			background: #eee;
+		}
+
+		&.is-success {
+			background: #0f0;
+		}
+
+		&.is-error {
+			background: #f00;
+		}
 	}
 
 	.images__img {
@@ -110,8 +198,6 @@
 		width: 100%;
 		height: 100%;
 		text-transform: capitalize;
-		color: #fff;
-		mix-blend-mode: exclusion;
 	}
 
 
@@ -128,12 +214,12 @@
 
 
 	.names__item {
-		transition: all 0.4s ease-in-out;
+		position: relative;
+		margin: 0.25em;
 	}
 
 	.names__btn {
-		display: inline-block;
-		margin: 0.25em;
+		display: block;
 		padding: 0.5em 1em;
 		font: inherit;
 		text-transform: capitalize;
@@ -141,9 +227,15 @@
 		border: 0;
 		-webkit-appearance: none;
 		cursor: grab;
+		border-radius: 5px;
 
 		&:active {
 			cursor: grabbing;
+		}
+
+		&:focus {
+			outline: none;
+			box-shadow: inset 0 0 0 2px #aaa;
 		}
 	}
 
